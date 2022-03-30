e8d3cff6d5dacfaebff35169d89d42b04c79c782
peregrinno <joseluan74@gmail.com>
2022-03-29 22:21:42 -0300

    Troquei a organização de classes para apenas uma.

--- src/App.java
+++ src/App.java
@@ -1,26 +1,12 @@
 // Orientação a Obejtos - Professor Jorge Fonseca
-class Pelicula {
-   int codigo_id_pelicula;
+//Alunos: Hydelbranda, Felipe, Luan, Moyses e Edson
+
+class Produto {
+   int id_produto;
+   String categoria_produto;
    String modelo;
    String tipo;
-}
- 
-class Capinha {
-   int codigo_id_cap;
-   String modelo;
-   String tipo;
-}
- 
-class Carregador {
-   int codigo_id_car;
-   String tipo;
-   String modelo;
-}
- 
-class Fone {
-   int codigo_id_fone;
-   String tipo;
-   String modelo;
+   Double valor_produto;
 }
  
 class Funcionario {
@@ -35,27 +21,28 @@ class Funcionario {
 class main {
    public static void main(String[] args) {
  
-       Pelicula pel1 = new Pelicula();
-       Capinha cap1 = new Capinha();
-       Carregador car1 = new Carregador();
-       Fone fone1 = new Fone();
+       Produto prod1 = new Produto();
        Funcionario fun1 = new Funcionario();
  
-       pel1.codigo_id_pelicula = 001;
-       pel1.tipo = "3D";
-       pel1.modelo = "M52 5G";
+       prod1.id_produto = 001;
+       prod1.categoria_produto = "Pelicula";
+       prod1.tipo = "3D";
+       prod1.modelo = "M52 5G";
  
-       cap1.codigo_id_cap = 101;
-       cap1.tipo = "Anti-impacto";
-       cap1.modelo = "M52 5G";
+       prod1.id_produto = 101;
+       prod1.categoria_produto = "Case";
+       prod1.tipo = "Anti-impacto";
+       prod1.modelo = "M52 5G";
  
-       car1.codigo_id_car = 201;
-       car1.tipo = "Portatil";
-       car1.modelo = "M52 5G";
+       prod1.id_produto = 201;
+       prod1.categoria_produto = "Carregador";
+       prod1.tipo = "Portatil";
+       prod1.modelo = "M52 5G";
  
-       fone1.codigo_id_fone = 301;
-       fone1.tipo = "Com Fio";
-       fone1.modelo = "M52 5G";
+       prod1.id_produto = 301;
+       prod1.categoria_produto = "Fone de ouvido";
+       prod1.tipo = "Com Fio";
+       prod1.modelo = "M52 5G";
  
        fun1.id_funcionario = 401;
        fun1.nome = "Hydelbranda Melo";
@@ -64,27 +51,28 @@ class main {
        fun1.endereco = "Rua do Grajau, 66";
        fun1.funcao = "Proprietária - Caixa";
  
-       Pelicula pel2 = new Pelicula();
-       Capinha cap2 = new Capinha();
-       Carregador car2 = new Carregador();
-       Fone fone2 = new Fone();
+       Produto prod2 = new Produto();
        Funcionario fun2 = new Funcionario();
  
-       pel2.codigo_id_pelicula = 002;
-       pel2.tipo = "PADRÃO";
-       pel2.modelo = "IPhone XR";
+       prod2.id_produto = 002;
+       prod2.categoria_produto = "Pelicula";
+       prod2.tipo = "PADRÃO";
+       prod2.modelo = "IPhone XR";
  
-       cap2.codigo_id_cap = 102;
-       cap2.tipo = "Silicone";
-       cap2.modelo = "IPhone XR";
+       prod2.id_produto = 102;
+       prod2.categoria_produto = "Case";
+       prod2.tipo = "Silicone";
+       prod2.modelo = "IPhone XR";
  
-       car2.codigo_id_car = 202;
-       car2.tipo = "Portatil";
-       car2.modelo = "iPhone XR";
+       prod2.id_produto = 202;
+       prod2.categoria_produto = "Carregador";
+       prod2.tipo = "Portatil";
+       prod2.modelo = "iPhone XR";
  
-       fone2.codigo_id_fone = 302;
-       fone2.tipo = "AIRDOT";
-       fone2.modelo = "IPhone XR";
+       prod2.id_produto = 302;
+       prod2.categoria_produto = "Fone de ouvido";
+       prod2.tipo = "AIRDOT";
+       prod2.modelo = "IPhone XR";
  
        fun2.id_funcionario = 402;
        fun2.nome = "José Felipe";
@@ -92,27 +80,28 @@ class main {
        fun2.data_nasc = "Quintas das Colinas humildes";
        fun2.funcao = "Vendedor";
  
-       Pelicula pel3 = new Pelicula();
-       Capinha cap3 = new Capinha();
-       Carregador car3 = new Carregador();
-       Fone fone3 = new Fone();
+       Produto prod3 = new Produto();
        Funcionario fun3 = new Funcionario();
  
-       pel3.codigo_id_pelicula = 003;
-       pel3.tipo = "HIDROGEL";
-       pel3.modelo = "Xiaomi Mi 11";
+       prod3.id_produto = 003;
+       prod3.categoria_produto = "Pelicula";
+       prod3.tipo = "HIDROGEL";
+       prod3.modelo = "Xiaomi Mi 11";
  
-       cap3.codigo_id_cap = 103;
-       cap3.tipo = "Aveludada";
-       cap3.modelo = "Xiaomi Mi 11";
+       prod3.id_produto = 103;
+       prod3.categoria_produto = "Case";
+       prod3.tipo = "Aveludada";
+       prod3.modelo = "Xiaomi Mi 11";
  
-       car3.codigo_id_car = 203;
-       car3.tipo = "Turbo";
-       car3.modelo = "Xiaomi Mi 11";
+       prod3.id_produto = 203;
+       prod3.categoria_produto = "Carregador";
+       prod3.tipo = "Turbo";
+       prod3.modelo = "Xiaomi Mi 11";
  
-       fone3.codigo_id_fone = 303;
-       fone3.tipo = "Headfone";
-       fone3.modelo = "Xiaomi Mi 11";
+       prod3.id_produto = 303;
+       prod3.categoria_produto = "Fone de ouvido";
+       prod3.tipo = "Headfone";
+       prod3.modelo = "Xiaomi Mi 11";
  
        fun3.id_funcionario = 403;
        fun3.nome = "Luan Araújo";
@@ -123,8 +112,8 @@ class main {
  
        System.out.println("");
  
-       System.out.println("Produto: " + pel1.codigo_id_pelicula + "\nModelo: " +
-               pel1.modelo + "\nTipo: " + pel1.tipo);
+       System.out.println("Produto: " + prod1.categoria_produto + "\nModelo: " +
+               prod1.modelo + "\nnTipo: " + prod1.tipo);
  
        System.out.println("");
    }
